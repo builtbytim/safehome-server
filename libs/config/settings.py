1bbd440657aafe7934673d66f4a2b5962fbd40fa
timmypelumy <timileyinpelumi.dev@gmail.com>
2023-10-28 17:23:01 +0100

    affiliates

--- libs/config/settings.py
+++ libs/config/settings.py
@@ -61,7 +61,7 @@ class Settings(BaseSettings):
     kek3: str = "kek3"
 
     allowed_origins: list[str] = [
-        "https://safehomecoop.com", "https://www.safehomecoop.com", "https://app.safehomecoop.com", "https://safehome-affiliates.vercel.app",
+        "https://safehomecoop.com", "https://www.safehomecoop.com", "https://app.safehomecoop.com", "https://safehomecoop-affiliates.vercel.app",
         "http://localhost:3000", "http://0.0.0.0:3000", "http://localhost:3001", "https://safehome-gg.vercel.app", "https://app.safehome.timmypelumy.dev", "https://safehome.timmypelumy.dev"]
 
 
