75129658464ed232e4eaef5313ca32936507ccb8
timmypelumy <timileyinpelumi.dev@gmail.com>
2024-06-22 13:18:06 +0100

    cors fix

--- libs/config/settings.py
+++ libs/config/settings.py
@@ -70,6 +70,7 @@ class Settings(BaseSettings):
         "https://safehomecoop.com", "https://www.safehomecoop.com", "https://app.safehomecoop.com", "https://safehomecoop-affiliates.vercel.app",
         "https://main.d259adcvsfh045.amplifyapp.com",
         "https://affiliates.safehomecoop.com",
+        "https://partners.safehomecoop.com",
         "http://localhost:3000", "http://0.0.0.0:3000", "http://localhost:3001", "https://safehome-gg.vercel.app", "https://app.safehome.timmypelumy.dev", "https://safehome.timmypelumy.dev"]
 
 
